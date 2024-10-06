503ff0a2f612033dab769cbab08799c05edabaca
Dinesh Kumar <mapmydestination@gmail.com>
2024-10-06 14:02:17 +0530

    updated

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
         </div>
         <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
             <picture class="icons">
-                 <img  width="20"  height="30" src="/img/icon-pen.svg" alt="icon-cv">
+                 <img  width="20"  height="30" src="./img/icon-pen.svg" alt="icon-cv">
             </picture>
             <br><br><br>
             <span class="text-center h4 tbold">
